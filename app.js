f689d5f0f400e3daa48e5b41b03057f693d634ae
JBoss <jboss@server.integration.redhat.com>
2018-06-13 13:46:18 +1200

    updated oc ip address

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(
 
     connection(mysql,{
 
-        host: '172.30.143.70', // host mysql service
+        host: '10.0.2.15', // host mysql service
         user: 'dbuser',
         password : 'password',
         port : 3306, // port mysql service
